c9ed70b06163630834940389a5da54a48332d5a8
inafu1137 <inafu1137@gmail.com>
2025-04-15 13:59:09 +0900

    RubyによるFizzBuzz機能の実装

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+(1..20).each do |i|
+    if i % 15 == 0
+      puts "FizzBuzz"
+    elsif i % 3 == 0
+      puts "Fizz"
+    elsif i % 5 == 0
+      puts "Buzz"
+    else
+      puts i
+    end
+  end
+  
