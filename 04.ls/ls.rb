66820972f392a0609bbcfb2e50246ce5c89fa126
inafu1137 <inafu1137@gmail.com>
2025-05-07 11:22:53 +0900

    ファイル名の最大幅に合わせて列幅を動的に設定

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -2,6 +2,7 @@
 # frozen_string_literal: true
 
 MAX_COLUMNS = 3
+MAX_WIDTH = 50
 
 def fetch_entries
   Dir.children('.').sort
@@ -17,8 +18,16 @@ def format_columns(entries, columns = MAX_COLUMNS)
     grid[row][col] = entry
   end
 
+  col_widths = Array.new(columns, 0)
+  columns.times do |col|
+    col_entries = grid.map { |row| row[col].to_s }
+    col_widths[col] = [col_entries.map(&:length).max || 0, MAX_WIDTH].min
+  end
+
   grid.each do |row|
-    puts row.map { |item| item.to_s.ljust(20) }.join
+    puts row.each_with_index.map { |item, i|
+      item.to_s.ljust(col_widths[i])
+    }.join(' ')
   end
 end
 
