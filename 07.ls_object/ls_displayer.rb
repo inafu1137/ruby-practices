8f3d682e6f8eb445433ffad41df7ef6d1bf6abca
inafu1137 <inafu1137@gmail.com>
2025-05-26 10:13:47 +0900

    LsDisplayerクラスのdisplayメソッドを修正し、nil行をスキップする機能を追加

--- 07.ls_object/ls_displayer.rb
+++ 07.ls_object/ls_displayer.rb
@@ -8,9 +8,15 @@ class LsDisplayer
 
   def display
     @grid.each do |row|
-      puts row.each_with_index.map { |item, i|
-        item.to_s.ljust(@col_widths[i])
-      }.join(' ')
+      next if row.nil?
+
+      if @col_widths.empty?
+        puts row.join(' ')
+      else
+        puts row.each_with_index.map { |item, i|
+          item.to_s.ljust(@col_widths[i])
+        }.join(' ')
+      end
     end
   end
 end
